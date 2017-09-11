d2bced3a572c26b2ac53a39f039d57d8c447869e
Unknown <joshquin@gmail.com>
2017-09-11 10:30:58 -0400

    Added local files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import DrizzleComponent from './DrizzleComponent.js'
+import drizzleConnect from './drizzleConnect.js'
+import DrizzleProvider from './DrizzleProvider.js'
+
+export {
+  DrizzleComponent,
+  drizzleConnect,
+  DrizzleProvider
+}
