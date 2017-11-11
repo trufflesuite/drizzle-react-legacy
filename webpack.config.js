f842b3ba579353b3bd5e9657e13e4fcac36ce3e1
Unknown <joshquin@gmail.com>
2017-11-10 20:21:20 -0500

    Web3 1.0 overhaul

--- webpack.config.js
+++ webpack.config.js
@@ -15,11 +15,12 @@ module.exports = {
       loader: 'babel-loader',
       options: {
         presets: ['react'],
-        plugins: [require('babel-plugin-transform-class-properties')]
+        plugins: [require('babel-plugin-transform-class-properties'), require('babel-plugin-transform-object-rest-spread')]
       }
     }]
   },
   externals: [
+    'drizzle',
     'prop-types',
     'react',
     'redux'
